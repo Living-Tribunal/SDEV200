3880da2374e8270cab8da31bdf05f8d7486cd99a
Cody Dalton <cdalton6@ivytech.edu>
2023-10-27 14:57:05 -0400

    message

--- M01/Main_M1_9_9.java
+++ M01/Main_M1_9_9.java
@@ -1,3 +1,5 @@
+
+
 public class Main_M1_9_9 {
     public static void main(String[] args){
         RegularPolygon firstPolygon = new RegularPolygon();
@@ -6,14 +8,11 @@ public class Main_M1_9_9 {
 
         RegularPolygon thirdPolygon = new RegularPolygon(10,4,5.6,7.8);
 
-        System.out.println("The perimeter of polygon one is " + firstPolygon.getPerimeter() + " the "
-                + "area of polygon one is " + firstPolygon.getArea());
+        System.out.println("The perimeter of polygon one is " + firstPolygon.getPerimeter() + " the " + "area of polygon one is " + firstPolygon.getArea());
 
-        System.out.println("The perimeter of polygon two is " + secondPolygon.getPerimeter() + " the "
-                + "area of polygon two is " + secondPolygon.getArea());
+        System.out.println("The perimeter of polygon two is " + secondPolygon.getPerimeter() + " the " + "area of polygon two is " + secondPolygon.getArea());
 
-        System.out.println("The perimeter of polygon three is " + thirdPolygon.getPerimeter() + " the "
-                + "area of polygon three is " + thirdPolygon.getArea());
+        System.out.println("The perimeter of polygon three is " + thirdPolygon.getPerimeter() + " the " + "area of polygon three is " + thirdPolygon.getArea());
             
             }
 }
